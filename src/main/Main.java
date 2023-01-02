0fdf96f44f82e2c08ee45e54f823c737c0d6d8f6
BuildTools <lukas.schimpf@gmail.com>
2023-01-02 11:32:23 +0100

    Minor changes to login automaton

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,8 +5,8 @@ public class Main {
 		String userId = "21ed7a53-ff36-4daf-8da0-c8b66b11c0de";
 		String driverPath = "C:\\ChromeDriver\\chromedriver.exe";
 		
-		//RunnerLogins runner = new RunnerLogins(userId, driverPath);
-		RunnerAlerts runner = new RunnerAlerts(userId);
+		RunnerLogins runner = new RunnerLogins(userId, driverPath);
+		//RunnerAlerts runner = new RunnerAlerts(userId);
 		System.out.println("Created Runner");
 		
 		runner.run();
--- src/main/RunnerLogins.java
+++ src/main/RunnerLogins.java
@@ -10,6 +10,7 @@ public class RunnerLogins {
 	private WebDriver driver;
 	private MarketAlertLogin marketAlertLogin;
 	private MarketAlertList marketAlertList;
+	private boolean isLoggedIn;
 
 	public RunnerLogins(String userId, String chromeDriverPath){
         //set path to ChromeDriver executable
@@ -19,26 +20,45 @@ public class RunnerLogins {
 		this.driver = new ChromeDriver();
 		this.marketAlertLogin = new MarketAlertLogin(driver);
 		this.marketAlertList = new MarketAlertList(driver);
+		this.isLoggedIn = false;
 	}
 
 	public void run(){
 		
-		int action = 0;
-		
+		//Loop forever
         while(true){
-			action = Util.randomInt(0,3);
-			
-			switch(action){
-                case 0:
-                    goToLoginPage();
-                    goodLogin();
-                    logout();
-                    break;
-                case 1:
-                	goToLoginPage();
-                    badLogin();
-                    break;
-            }
+        	
+        	//if system is logged in
+			if(!isLoggedIn){
+				switch(Util.randomInt(0,3)){
+            		case 0:
+            			//good login
+            			goToLoginPage();
+            			goodLogin();
+            			break;
+            		case 1:
+            			//bad login
+            			goToLoginPage();
+            			badLogin();
+            			break;
+            		case 2:
+            			//verify login status
+            			loginCheck();
+            			break;
+				}
+			//if system is logged out
+			}else{
+				switch(Util.randomInt(0,2)){
+        		case 0:
+        			//log out
+        			logout();
+        			break;
+        		case 1:
+        			//verify login status
+        			loginCheck();
+        			break;
+				}
+			}
 			
 			try {
 				Thread.sleep(1000);
@@ -56,12 +76,14 @@ public class RunnerLogins {
 	public void goodLogin(){
 		System.out.println("Good login at: " + System.currentTimeMillis());
 		login(userId);
+		isLoggedIn = true;
 	}
 
 	// Logs in with a bad user Id
 	public void badLogin(){
 		System.out.println("Bad login at: " + System.currentTimeMillis());
 		login("Bad Id");
+		isLoggedIn = false;
 	}
 	
 	// Attempts to log into the system with the given userId
@@ -74,9 +96,19 @@ public class RunnerLogins {
 	public void logout(){
 		System.out.println("Logout at: " + System.currentTimeMillis());
 		marketAlertList.logOut();
+		isLoggedIn = false;
 	}
 	
-	public boolean loginCheck(){
+	public void loginCheck(){
+		//Empty method that triggers the larva script to check the login
+		System.out.println("Checking Login Status");
+		return;
+	}
+	
+	//Returns whether the system is logged in based on whether it is on the Alerts Page or not
+	/*
+	public boolean isLoggedIn(){
 		return marketAlertList.isOnAlertsPage();
 	}
+	*/
 }
